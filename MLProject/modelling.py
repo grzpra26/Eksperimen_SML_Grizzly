0adc31f3c8ea2d96b13b455213ac876685b76d59
grzpra26 <104147671+grzpra26@users.noreply.github.com>
2026-09-09 16:55:32 +0700

    update modelling and modelling tuning script

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,3 +1,6 @@
+import os
+os.environ["MLFLOW_ALLOW_FILE_STORE"] = "true"
+
 import pandas as pd
 import mlflow
 import mlflow.sklearn
@@ -5,31 +8,45 @@ import dagshub
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-# 1. Inisialisasi ke DagsHub
+# 1. Inisialisasi DagsHub untuk ambil URI online
 dagshub.init(
     repo_owner='grzpra26', 
     repo_name='Eksperimen_SML_Grizzly', 
     mlflow=True
 )
+dagshub_uri = mlflow.get_tracking_uri()
 
-# 2. Mengaktifkan Autolog (Syarat Wajib Kriteria Basic untuk modelling.py)
-mlflow.autolog()
-
-# 3. Memuat Dataset Preprocessing
+# 2. Memuat Dataset
 data_path = "diabetes_preprocessing.csv"
 df = pd.read_csv(data_path)
 
 X = df.drop(columns=['diabetes'])
 y = df['diabetes']
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-# 4. Setup Eksperimen
+# Path absolut ke folder mlruns lokal
+local_path = os.path.abspath("./mlruns")
+
+# --- STEP A: LOGGING KE LOCAL ---
+print("Logging ke Local mlruns...")
+os.environ["MLFLOW_TRACKING_URI"] = f"file:///{local_path}"
+mlflow.set_tracking_uri(f"file:///{local_path}")
+mlflow.autolog()
 mlflow.set_experiment("Diabetes_Classification_Grizzly")
 
 with mlflow.start_run(run_name="Baseline_Random_Forest"):
-    # Model sederhana tanpa Hyperparameter Tuning
     model = RandomForestClassifier(n_estimators=50, max_depth=5, random_state=42)
     model.fit(X_train, y_train)
-    
-    print("Baseline Model berhasil dilatih menggunakan Autolog! 🚀")
+
+# --- STEP B: LOGGING KE DAGSHUB ---
+print("Logging ke DagsHub Online...")
+os.environ["MLFLOW_TRACKING_URI"] = dagshub_uri
+mlflow.set_tracking_uri(dagshub_uri)
+mlflow.autolog()
+mlflow.set_experiment("Diabetes_Classification_Grizzly")
+
+with mlflow.start_run(run_name="Baseline_Random_Forest"):
+    model = RandomForestClassifier(n_estimators=50, max_depth=5, random_state=42)
+    model.fit(X_train, y_train)
+
+print("Baseline Model Sukses Dihasilkan di Local & DagsHub! 🚀")
