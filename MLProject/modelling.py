bcd73dc497e663b813c7541597d5d7db1bb1cfb2
grzpra26 <104147671+grzpra26@users.noreply.github.com>
2026-09-09 16:59:19 +0700

    [skip ci]

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -24,26 +24,32 @@ X = df.drop(columns=['diabetes'])
 y = df['diabetes']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-# Path absolut ke folder mlruns lokal
 local_path = os.path.abspath("./mlruns")
 
 # --- STEP A: LOGGING KE LOCAL ---
-print("Logging ke Local mlruns...")
+print("Simpan ke Local mlruns...")
 os.environ["MLFLOW_TRACKING_URI"] = f"file:///{local_path}"
 mlflow.set_tracking_uri(f"file:///{local_path}")
 mlflow.autolog()
 mlflow.set_experiment("Diabetes_Classification_Grizzly")
+
+# Hapus ID Run bawaan mlflow run agar tidak crash di local
+os.environ.pop("MLFLOW_RUN_ID", None)
+os.environ.pop("MLFLOW_EXPERIMENT_ID", None)
 
 with mlflow.start_run(run_name="Baseline_Random_Forest"):
     model = RandomForestClassifier(n_estimators=50, max_depth=5, random_state=42)
     model.fit(X_train, y_train)
 
 # --- STEP B: LOGGING KE DAGSHUB ---
-print("Logging ke DagsHub Online...")
+print("Kirim ke DagsHub Online...")
 os.environ["MLFLOW_TRACKING_URI"] = dagshub_uri
 mlflow.set_tracking_uri(dagshub_uri)
 mlflow.autolog()
 mlflow.set_experiment("Diabetes_Classification_Grizzly")
+
+os.environ.pop("MLFLOW_RUN_ID", None)
+os.environ.pop("MLFLOW_EXPERIMENT_ID", None)
 
 with mlflow.start_run(run_name="Baseline_Random_Forest"):
     model = RandomForestClassifier(n_estimators=50, max_depth=5, random_state=42)
